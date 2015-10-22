c1b49f700868d062072a184dfd48113b2a3ae299
Christopher Aravena <caravena@nisum.com>
2015-10-22 18:08:03 -0300

    First test with cucumber

--- src/test/java/com/demo/cucumber/CreateStepdefs.java
+++ src/test/java/com/demo/cucumber/CreateStepdefs.java
@@ -1,5 +1,50 @@
 package com.demo.cucumber;
 
+import static org.junit.Assert.assertNotNull;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import com.demo.domain.Person;
+import com.google.gson.Gson;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class CreateStepdefs {
 
+    Person[] persons;
+
+    @Given("^I have a valid person (.*), (.*), (.*)$")
+    public void I_have_a_valid_person(String name, int age, String description) {
+        persons[0] = new Person();
+        persons[0].setName(name);
+        persons[0].setAge(age);
+        persons[0].setDescription(description);
+    }
+
+    @When("^I create the person$")
+    public void I_create_the_person() {
+        Gson json = new Gson();
+        String url = RunAllTest.URL_BASE + "/person/create";
+        String requestJson = json.toJson(persons[0]);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> entity = new HttpEntity<String>(requestJson, headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        persons[1] = restTemplate.postForObject(url, entity, Person.class);
+    }
+
+    @Then("^I should see it persisted$")
+    public void I_should_see_it_persisted() {
+        RestTemplate restTemplate = new RestTemplate();
+        Person person = restTemplate.getForObject(RunAllTest.URL_BASE + "/get" + persons[1].getId(), Person.class);
+        assertNotNull(person);
+    }
 }
---
+++ src/test/java/com/demo/cucumber/RunAllTest.java
@@ -0,0 +1,10 @@
+package com.demo.cucumber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+public class RunAllTest {
+    public static final String URL_BASE = new String("http://localhost:8080/rest");
+}
